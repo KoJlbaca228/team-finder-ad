e11d9a8abe2d8f47f5d692cc795e0d415b6c05eb
Kojlbaca228 <araresk3@gmail.com~>
2026-07-23 16:26:55 +0300

    Configure project administration

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "status", "created_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("name", "description", "owner__email")
+    list_select_related = ("owner",)
+    autocomplete_fields = ("owner",)
+    filter_horizontal = ("participants",)
+    readonly_fields = ("created_at",)
+    fields = (
+        "name",
+        "description",
+        "owner",
+        "github_url",
+        "status",
+        "participants",
+        "created_at",
+    )
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,94 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import BaseUserCreationForm, UserChangeForm
 
-# Register your models here.
+from .models import User
+
+
+class AdminUserCreationForm(BaseUserCreationForm):
+    class Meta(BaseUserCreationForm.Meta):
+        model = User
+        fields = (
+            "email",
+            "name",
+            "surname",
+            "phone",
+            "github_url",
+            "about",
+        )
+
+
+class AdminUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = "__all__"
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_form = AdminUserCreationForm
+    form = AdminUserChangeForm
+
+    list_display = (
+        "email",
+        "name",
+        "surname",
+        "phone",
+        "is_active",
+        "is_staff",
+    )
+    list_editable = ("is_active",)
+    list_filter = ("is_active", "is_staff", "is_superuser", "groups")
+    search_fields = ("email", "name", "surname", "phone")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Профиль",
+            {
+                "fields": (
+                    "name",
+                    "surname",
+                    "avatar",
+                    "about",
+                    "phone",
+                    "github_url",
+                )
+            },
+        ),
+        ("Избранное", {"fields": ("favorites",)}),
+        (
+            "Права доступа",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Вход в систему", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "name",
+                    "surname",
+                    "phone",
+                    "github_url",
+                    "about",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
+    readonly_fields = ("last_login",)
+    filter_horizontal = ("favorites", "groups", "user_permissions")
